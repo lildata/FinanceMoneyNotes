ad08ebb5f404c050aa5204d45b7870b01992eb04
Programming is fun <lildata@users.noreply.github.com>
2015-07-30 23:03:29 +0200

    Create CentralBanks.scala

---
+++ src/main/scala/CentralBanks.scala
@@ -0,0 +1,18 @@
+package FinancialAnalysis
+
+object CentralBank {
+
+}
+
+object FederalReserveSystem extends CentralBank {
+
+
+
+
+  Object FederalOpenMarketCommittee {
+  
+  }
+
+}
+
+case class FederalReserveBank(district:String)
