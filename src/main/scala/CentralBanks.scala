8f704c010170a63a267c06b2a2a0d9876763e3a5
Programming is fun <lildata@users.noreply.github.com>
2015-07-30 23:38:44 +0200

    Update CentralBanks.scala

--- src/main/scala/CentralBanks.scala
+++ src/main/scala/CentralBanks.scala
@@ -1,18 +1,34 @@
 package FinancialAnalysis
 
-object CentralBank {
+trait CentralBank {
 
 }
 
 object FederalReserveSystem extends CentralBank {
 
+  def setDiscountRate(when:Date) = new DiscountRate(when)
 
 
 
-  Object FederalOpenMarketCommittee {
-  
+  object FederalOpenMarketCommittee {
+    def setTargetInterbankRate(when:Date) = new FederalFundsRate(when)
   }
 
 }
 
 case class FederalReserveBank(district:String)
+
+class DiscountRate(when:Date) extends InterestRate { //or base rate, or repo rate
+  val defintion = """
+    instrument of monetary policy that set the rate at which eligible institutions 
+    can borrow money from the central bank, usually on a short-term basis, 
+    to meet temporary shortages of liquidity"""
+}
+
+trait InterbankRate extends InterestRate
+
+class FederalFundsRate(when:Date) extends InterbankRate
+
+class EuroInterbankOfferedRate(when:Date) extends InterbankRate //Euribor
+
+class LondonInterbankOfferedRate(when:Date) extends InterbankRate //LIBOR
