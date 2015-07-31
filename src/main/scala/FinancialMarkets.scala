d8c536d491f5888341a91971561058fd957f01fb
Programming is fun <lildata@users.noreply.github.com>
2015-07-31 02:14:55 +0200

    Update FinancialMarkets.scala

--- src/main/scala/FinancialMarkets.scala
+++ src/main/scala/FinancialMarkets.scala
@@ -1,27 +1,27 @@
-Object FinancialMarket {
+trait FinancialMarket {
     val def_ = ""
     val goal = "allow to trade fungible items of value at low transaction costs and at prices that reflect supply and demand"
 }
 
-Object StockMarket extends FinancialMarket {
+object StockMarket extends FinancialMarket {
     val syn = "Equity Market"::Nil
 }
 
-Object BondMarket extends FinancialMarket {
+object BondMarket extends FinancialMarket {
     val syn = "Debt Market"::"Credit Market"::Nil
     val def_ = "" 
     val goal = "primary goal is to provide long-term funding for public and private expenditures"
 }
 
-Object DerivativesMarket extends FinancialMarket {
+object DerivativesMarket extends FinancialMarket {
     val def_ = """financial market for derivatives, financial instruments like futures contracts or options, which are derived from other forms of assets."""
 }
 
-Object CommodityMarket extends FinancialMarket {
+object CommodityMarket extends FinancialMarket {
 
 }
 
-Object ForeignExchangeMarket extends FinancialMarket {
+object ForeignExchangeMarket extends FinancialMarket {
     val syn = "forex"::"FX"::"Currency market"::Nil
     val def_ = """The Foreign Exchange Market is a global decentralized market for the trading of currencies. 
                 In terms of volume of trading, it is by far the largest market in the world.
@@ -29,15 +29,15 @@ Object ForeignExchangeMarket extends FinancialMarket {
                 It assists international trade and investments by enabling currency conversion."""
 }
 
-Object Terminology {
+object Terminology {
     val definitions = Map(
         "floating exchange rate" -> """(fluctuating exchange rate) is a type of exchange-rate regime in which a currency's value is allowed to fluctuate in response to market mechanisms of the foreign-exchange market.""", 
         "floating currency" -> """A currency that uses a floating exchange rate."""
     )
 }
 
-Object MarketLiquidity {
-    Object Metrics {
+object MarketLiquidity {
+    object Metrics {
         
         /**
          * Difference between the prices quoted for an immediate sale (bid) and an immediate purchase (offer)
@@ -68,8 +68,7 @@ Object MarketLiquidity {
          * Indirect measure of the liquidity
          */
         var heldByInstitutions:Percent
-        
-        
+
     }
 }
 
