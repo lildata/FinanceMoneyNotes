f3d26a0cbfe31eec9acb0a21b1d3b8eb8743ad5d
Programming is fun <lildata@users.noreply.github.com>
2015-08-30 21:25:45 +0200

    Update merge-acquisition.scala

--- src/main/scala/merge-acquisition.scala
+++ src/main/scala/merge-acquisition.scala
@@ -3,7 +3,7 @@ object MA {
   /**
    * @param Vb the present value of the bidding firm's cash flows in the absence of the aquisition
    * @param Vt the present value of the target firm's cash flows in the absence of the aquisition
-   * @param Vb the present value of the merged firms' cash flows following the aquisition
+   * @param Vbt the present value of the merged firms' cash flows following the aquisition
    * @return Synergistic benefits from acquisition
    */
   def GAIN(Vb:Float, Vt:Float, Vbt:Float) = Vbt - (Vb + Vt) 
